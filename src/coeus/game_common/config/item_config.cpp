ee9c0cb464155510785909ffde1e2b3f85ab11a0
AngryPowman <angrypowman@qq.com>
2014-06-05 01:15:59 +0800

    Added ItemConfig()

---
+++ src/coeus/game_common/config/item_config.cpp
@@ -0,0 +1,15 @@
+#include "item_config.h"
+
+ItemConfig::ItemConfig()
+{
+}
+
+
+ItemConfig::~ItemConfig()
+{
+}
+
+bool ItemConfig::parse()
+{
+    return true;
+}
---
+++ src/coeus/game_common/config/item_config.h
@@ -0,0 +1,40 @@
+#ifndef __ITEM_CONFIG_H__
+#define __ITEM_CONFIG_H__
+
+#include "configuration.h"
+#include "../item_definition.h"
+
+struct ItemData
+{
+    uint32 id;
+    uint32 type;
+    std::string name;
+    std::string description;
+    uint16 maxStack;
+    uint32 flags;
+
+#if defined(COEUS_CLIENT)
+    std::string icon;
+#endif
+};
+
+typedef adap_map<uint32, ItemData> ItemTable;
+class ItemConfig
+    : public Configuration, public Venus::Singleton<ItemConfig>
+{
+public:
+    ItemConfig();
+    virtual ~ItemConfig();
+
+public:
+    bool parse();
+
+private:
+    ItemTable _items;
+};
+
+#endif // !__ITEM_CONFIG_H__
+
+
+
+
