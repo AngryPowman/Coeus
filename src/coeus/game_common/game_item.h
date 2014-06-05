296bb3ed514a8ef79597ca0411eef041f443bfd5
AngryPowman <angrypowman@qq.com>
2014-06-06 00:17:07 +0800

    Load from item config

--- src/coeus/game_common/game_item.h
+++ src/coeus/game_common/game_item.h
@@ -4,44 +4,38 @@
 #include <exception>
 #include <venus_net/venus_net.h>
 #include "config/item_config.h"
-#include "item_exception.h"
 #include "item_helper.h"
 
 class ItemData;
 class GameItem
 {
 public:
-    GameItem(const ItemData& itemData)
+    GameItem(const ItemData* itemData)
         : _itemData(itemData), _count(0)
     {
-        if (itemData.maxStack <= 0)
-        {
-            throw BadStackValueException();
-        }
-
-        _count = 0;
+        _count = 1;
     }
 
     virtual ~GameItem() {}
 
 public:
-    inline const uint32 id() const { return _itemData.id; }
-    inline const uint32 type() const { return _itemData.type; }
-    inline const std::string& typeName() const { return ItemHelper::ItemType::toTypename(_itemData.type); }
-    inline const std::string& name() const { return _itemData.name; }
-    inline const std::string& description() const { return _itemData.description; }
+    inline const uint32 id() const { return _itemData->id; }
+    inline const uint32 type() const { return _itemData->type; }
+    inline const std::string& typeName() const { return ItemHelper::ItemType::toTypename(_itemData->type); }
+    inline const std::string& name() const { return _itemData->name; }
+    inline const std::string& description() const { return _itemData->description; }
     inline void setCount(uint16 count) { _count = count; }
     inline int count() const { return _count; }
-    inline int maxStack() const { return _itemData.maxStack; }
-    uint32 flags() const { return _itemData.flags; }
+    inline int maxStack() const { return _itemData->maxStack; }
+    inline uint32 flags() const { return _itemData->flags; }
 
 #if defined(COEUS_CLIENT)
 public:
-    const std::string& icon() const{ return _itemData.icon; }
+    const std::string& icon() const{ return _itemData->icon; }
 #endif
 
 private:
-    const ItemData& _itemData;
+    const ItemData* _itemData;
     uint16 _count;
 };
 
